7aa25179643e610cf1ec0dfe07eb4e50d16f3f7e
divyang parmar <parmar84divyang@gmail.com>
2026-04-28 13:46:49 +0530

    new update

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get("/", (req, res) => {
+  res.send("Server is live 🚀");
+});
+
 // Routes : 
 app.use("/api/music", Routers)
-
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server Started at PORT : ${PORT}`);
